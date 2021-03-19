ea81ae7feebdce9bbc8037d6d18c4ebfca3d6364
Dezser Bertalan <dezsber@gmail.com>
2021-03-19 23:30:12 +0100

    kis kontrollerezés

--- Skeleton/src/logic/Controler.java
+++ Skeleton/src/logic/Controler.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Controler {
     ///Singleton stuff
     private static Controler instance;
+    private boolean gameIsOn;
 
     private Controler()
     {}
@@ -46,11 +47,27 @@ public class Controler {
     public void initGame()
     {}
 
+    /**
+     * Elindítja és futtatja a játékot.
+     * 
+     */
     public void startGame()
-    {}
+    {
+        gameIsOn = true;
+        while(gameIsOn){
 
+            space.step();
+            for(Robot r : robots){ r.step();}
+            for(Settler s : settlers){s.step();}
+        }
+    }
+    /**
+     * leállítja a játkot.
+     */
     public void endGame()
-    {}
+    {   
+        gameIsOn = false;
+    }
 
     /**
      * a paraméterként megkapott robotot hozzáadja a robots listájához
@@ -62,15 +79,22 @@ public class Controler {
     }
 
     /**
-     *  ha egy telepes meghal, akkor meghívja  ezt a Settler_die() függvényt, 
+     * Jegyzi egy telepes halálát
+     * 
+     * Ha egy telepes meghal, akkor meghívja  ezt a Settler_die() függvényt, 
      * ami eltávolítja a settlers listából a paraméterként kapott Settler objektumot
      * és átírja a Settler_alive változó értékét.
+     * Ha nem marad több settler, játék végét hív.
+     * 
      * @param s
      */
     public void settlerDie(Settler s)
     {
         settlers.remove(s);
         settlersAlive--;
+
+        if(settlers.size()==0)
+            endGame();
     }
 
     /**
@@ -81,10 +105,5 @@ public class Controler {
     public void robotDie(Robot r)
     {
         robots.remove(r);
-    }
-
-    public void settlerDie(Settler s)
-    {
-
     }
 }
