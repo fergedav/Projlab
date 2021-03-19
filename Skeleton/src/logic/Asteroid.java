a958e5c19edcaf48b5594fcae0150f2fb4261fd4
aront22 <78855764+aront22@users.noreply.github.com>
2021-03-19 19:07:56 +0100

    Commentelve

--- Skeleton/src/logic/Asteroid.java
+++ Skeleton/src/logic/Asteroid.java
@@ -2,9 +2,19 @@ package logic;
 
 public class Asteroid extends Orbit {
 
+    /**
+     * Az aszteroida magja
+     */
     private Resource core;
     //private boolean inSunlight = false;
 
+    /**
+     * Új Aszteroida
+     * @param _x kezdő x hely
+     * @param _y kezdő y hely
+     * @param l rétegek száma
+     * @param c Nyers anyag ami tartalmaz, lehet null ha üres
+     */
     public Asteroid(int _x, int _y, int l, Resource c)
     {
         x = _x;
@@ -18,6 +28,10 @@ public class Asteroid extends Orbit {
         layers = l;
     }
 
+    /**
+     * Megvizsgálja, hogy az aszteroida napközelben van-e illetve, hogy teljesen át van e fúrva a kérge. 
+     * Ha mind a kettő igaz, meghívja a magja (a tárolt resource objektumára) reaction() függvényét.
+     */
     private void reaction()
     {
         if(inLight && core != null && layers == 0)
@@ -25,6 +39,9 @@ public class Asteroid extends Orbit {
 
     }
 
+    /**
+     * Robbanás esetén meghívja az összes jelenleg rajta tartózkodó utazóra a Explosion() metódust.
+     */
     public void explosion()
     {
         for (Traveler t : travelers) {
@@ -32,6 +49,10 @@ public class Asteroid extends Orbit {
         }
     }
 
+    /**
+     * Az aszteroidán végeznek ásást. Eggyel csökkenti a layers értékét. 
+     * Ha a layers értéke eléri a 0-át, akkor meghívja az reaction() privát metódusát az aszteroidának.
+     */
     public void drilled()
     {
         if(layers != 0)
@@ -44,6 +65,12 @@ public class Asteroid extends Orbit {
         }
     }
 
+    /**
+     * A hívó visszakapja az aszteroida magját. 
+     * Ha a layers értéke 0, akkor visszatér a magjában található nyersanyaggal, az új core érték null. 
+     * Ha a layers értéke 0-tól különböző akkor null-lal tér vissza.
+     * @return Resource 
+     */
     @Override
     public Resource retrieveResource()
     {
@@ -56,6 +83,14 @@ public class Asteroid extends Orbit {
         return c;
     }
 
+    /**
+     * Visszateszik az üres aszteroidába a nyersanyagot. 
+     * A paraméterként kapott Resource-t elhelyezi a core-ba, ha az null. 
+     * Visszatérési értéke true, ha eredetileg null volt a core, amúgy false. 
+     * Ha az aszteroida éppen napfényben van, ellenőrzi, hogy az újonnan betett anyagnak van e bármilyen reakciója.
+     * @param r nyersanyag amit leraknak
+     * @return sikeres volt a nyersanyag lerakása
+     */
     @Override
     public boolean putResource(Resource r)
     {
@@ -72,6 +107,10 @@ public class Asteroid extends Orbit {
         return true;
     }
 
+    /**
+     * Napvihar esetén megvizsgálja, hogy a layers értéke 0-e és hogy a core értéke null. 
+     * Ha nem akkor az összes jelenleg rajta tartózkodó utazóra a Die() metódust hívja.
+     */
     @Override
     public void sunstormArrive()
     {
@@ -83,19 +122,21 @@ public class Asteroid extends Orbit {
         }
     }
 
+    /**
+     * Eldönti, hogy a koordinátái alapján beleesik-e a jelzett négyzetbe. 
+     * Ha igen, akkor beállítja true-ra a inLight attribútumot és meghívja a magára a privát Reaction() metódust. 
+     * Ha nem esik bele, akkor beállítja false-ra a inLight attribútumot.
+     * @param x1 left
+     * @param y1 top
+     * @param x2 right
+     * @param y2 bottom
+     */
     @Override
     public void sunLightArrive(int x1, int y1, int x2, int y2)
     {
-        if(x >= x1 && y >= y1 &&
-            x <= x2 && y <= y2)
-        {
-            inLight = true;
+        super.sunLightArrive(x1, y1, x2, y2);
+        if(inLight)
             reaction();
-        }
-        else
-        {
-            inLight = false;
-        }
 
     }
 }
--- Skeleton/src/logic/Orbit.java
+++ Skeleton/src/logic/Orbit.java
@@ -10,50 +10,93 @@ public abstract class Orbit {
     protected int x; //senki nem tárolja a koordinátákat tömbben xd
     protected int y;
 
+    /**
+     * Virtuális függvény. 
+     * Hozzáadja a paraméterként kapott Traveler-t az Orbit travelers listájához és visszatér önmagával.
+     * @param t érkezett traveler
+     * @return saját maga
+     */
     public Orbit addTraveler(Traveler t)
     {
         travelers.add(t);
         return this;
     }
 
+    /**
+     * Visszatér az Orbiton jelenleg tartózkodó utazók listájával
+     * @return Orbiton jelenleg tartózkodó utazók
+     */
     public List<Traveler> getTravelers()
     {
         return travelers;
     }
 
+    /**
+     * Eltávolítja a paraméterként kapott Traveler-t az Orbit travelers listájából.
+     * @param t Traveler
+     */
     public void removeTraveler(Traveler t)
     {
         travelers.remove(t);
     }
 
+    /**
+     * A paraméterként kapott Orbit-ot hozzáadja a szomszédsági listájához.
+     * @param o Orbit
+     */
     public void addNeighbour(Orbit o)
     {
         neighbours.add(o);
     }
 
+    /**
+     * Kitörli a paraméterként kapott Orbitot az objektum szomszédsági listájából.
+     * @param o Orbit
+     */
     public void removeNeighbour(Orbit o)
     {
         neighbours.remove(o);
     }
 
+    /**
+     * Visszatér az orbit i-edik szomszédjával. Ha i-edik szomszéd nem létezik null-t ad vissza.
+     * @param i szomszéd sorszáma
+     * @return szomszédos Orbit
+     */
     public Orbit getNeighbour(int i)
     {
-        return neighbours.get(i);
+        try {
+            return neighbours.get(i);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     public void drilled()
     { }
 
+    /**
+     * visszadja a tartalmazott nyersanyagot
+     * @return Resource 
+     */
     public Resource retrieveResource()
     {
         return null;
     }
 
+    /**
+     * Virtuális függvény. Alapértelmezésben false-t ad vissza.
+     * @param r
+     * @return
+     */
     public boolean putResource(Resource r)
     {
         return false;
     }
 
+    /**
+     * Alapértelmezésben meghívja a travelers listája összes elemére a Die() függvényt.
+     */
     public void sunstormArrive()
     {
         for (Traveler t : travelers) {
@@ -61,13 +104,31 @@ public abstract class Orbit {
         }
     }
 
+    /**
+     * Eldönti, hogy a koordinátái alapján beleesik-e a jelzett négyzetbe. 
+     * Ha igen, akkor beállítja true-ra a inLight attribútumot, ha nem beállítja false-ra.
+     * @param x1 left
+     * @param y1 top
+     * @param x2 right
+     * @param y2 bottom
+     */
     public void sunLightArrive(int x1, int y1, int x2, int y2)
     {
-        for (Traveler t : travelers) {
-            t.die();
+        if(x >= x1 && y >= y1 &&
+            x <= x2 && y <= y2)
+        {
+            inLight = true;
+        }
+        else
+        {
+            inLight = false;
         }
     }
 
+    /**
+     * Még nem átfurt rétegek száma
+     * @return rétegek
+     */
     public int getLayers()
     {
         return layers;
