f681ad7c20268830210b38b5544e04689dd9cc76
Dezser Bertalan <dezsber@gmail.com>
2021-03-20 16:38:10 +0100

    3.1/2/3/4 done

--- Skeleton/src/skeleton/SkeletonMain.java
+++ Skeleton/src/skeleton/SkeletonMain.java
@@ -339,12 +339,12 @@ public class SkeletonMain {
     {
         Logger.formatPrint("Init");
 
-        Asteroid a1 = new Asteroid(1,1,10,new Iron());
-        Settler s1 = new Settler(a1);
+        Asteroid asteroid = new Asteroid(1,1,10,new Iron());
+        Settler settler = new Settler(asteroid);
 
         Logger.formatPrint("_3_1_MiningOnNotFullyDrilledAsteroid");
 
-        s1.mining();
+        settler.mining();
 
         Logger.formatPrint("Teszt Vege");
     }
@@ -352,12 +352,12 @@ public class SkeletonMain {
     {
         Logger.formatPrint("Init");
 
-        Asteroid a1 = new Asteroid(1,1,0,new Iron());
-        Settler s1 = new Settler(a1);
+        Asteroid asteroid = new Asteroid(1,1,0,new Iron());
+        Settler settler = new Settler(asteroid);
 
-        Logger.formatPrint("_3_2_MiningOnNotFullyDrilledAsteroid");
+        Logger.formatPrint("_3_2_MiningIronOnFullyDrilledAsteroid");
 
-        s1.mining();
+        settler.mining();
 
         Logger.formatPrint("Teszt Vege");
     }
@@ -365,12 +365,12 @@ public class SkeletonMain {
     {
         Logger.formatPrint("Init");
 
-        Asteroid a1 = new Asteroid(1,1,0,null);
-        Settler s1 = new Settler(a1);
+        Asteroid asteroid = new Asteroid(1,1,0,null);
+        Settler settler = new Settler(asteroid);
 
-        Logger.formatPrint("_3_3_MiningOnNotFullyDrilledAsteroid");
+        Logger.formatPrint("_3_3_MiningOnFullyDrilledButEmptyAsteroid");
 
-        s1.mining();
+        settler.mining();
 
         Logger.formatPrint("Teszt Vege");
     }
@@ -378,7 +378,12 @@ public class SkeletonMain {
     {        
         Logger.formatPrint("Init");
 
+        Stargate stargate = new Stargate();
+        Settler settler = new Settler(stargate);
+
         Logger.formatPrint("_3_4_MiningOnStargate");
+
+        settler.mining();
 
         Logger.formatPrint("Teszt Vege");
     }
