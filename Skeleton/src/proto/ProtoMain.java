180130df496211b8cef24d6fa07f8d4d71ad836d
aront22 <78855764+aront22@users.noreply.github.com>
2021-04-09 17:30:37 +0200

    Szerializálás

--- Skeleton/src/proto/ProtoMain.java
+++ Skeleton/src/proto/ProtoMain.java
@@ -1,8 +1,51 @@
 package proto;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import logic.*;
+
 public class ProtoMain {
+
     public static void main(String[] args) 
     {
-        
+        Controller c = Controller.getInstance();
+
+        Asteroid a = new Asteroid(0, 0, 0, new Uran(), false);
+        c.addOrbit(a);
+        Settler s = new Settler(a);
+        c.addSettler(s);
+        s.mining();
+        int x = 0;
+
+        try {
+            FileOutputStream fileOut =
+            new FileOutputStream("test.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(c);
+            out.close();
+            fileOut.close();
+            System.out.printf("Serialized data is saved in test.ser");
+         } catch (IOException i) {
+            i.printStackTrace();
+         }
+
+         try {
+            FileInputStream fileIn = new FileInputStream("test.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            Controller c2 = (Controller) in.readObject();
+            in.close();
+            fileIn.close();
+         } catch (IOException i) {
+            i.printStackTrace();
+            return;
+         } catch (ClassNotFoundException e) {
+            System.out.println("Employee class not found");
+            e.printStackTrace();
+            return;
+         }
     }
 }
