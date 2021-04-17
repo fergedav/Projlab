a481ab477c85ddd66c97bd3e56c3a864ed4b1f40
dezsber <78563926+SaintMichaelArchangel@users.noreply.github.com>
2021-04-17 09:24:42 +0200

    auto run all tests added.

--- Skeleton/src/proto/ProtoMain.java
+++ Skeleton/src/proto/ProtoMain.java
@@ -10,16 +10,29 @@ public class ProtoMain {
     public static void main(String[] args) 
     {
       BufferedReader r = new BufferedReader (new InputStreamReader (System.in));
-      while(true)
+      boolean automode = false;
+      if(!automode)
       {
-         try {
-            String line = r.readLine();
-            CommandHandler.processCommand(line);
-         } catch (Exception e) {
-            //TODO: handle exception
+         while(true)
+         {
+            try {
+               String line = r.readLine();
+               CommandHandler.processCommand(line);
+            } catch (Exception e) {
+               //TODO: handle exception
+            }  
          }
-         
       }
+      if(automode)
+      {
+         for (int i = 0; i < 50; i ++)
+         {
+            String command = "loadTest Teszt"+i+".txt Out"+i+".txt";
+            System.out.println(command);
+            CommandHandler.processCommand(command);
+         }
+      }
+      
   
     }
 }
